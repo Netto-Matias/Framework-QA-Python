6030adb351418ad146d10c969eea8def9977c71d
Netto-Matias <nettomatias2024@gmail.com>
2025-12-16 23:28:15 -0300

    Configurado CI/CD con GitHub Actions y modo Headless

--- tests/test_ui/conftest.py
+++ tests/test_ui/conftest.py
@@ -1,11 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")
 def driver():
     print("\n[SETUP] Creando instancia del driver...")
+    
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+    
     d = webdriver.Chrome()
-    d.maximize_window()
     
     yield d
     
